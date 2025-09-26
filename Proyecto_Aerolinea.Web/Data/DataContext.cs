57e5f4283da544e842819f20127f511ae2b249a5
Jose Cardenas <josecardenas0045@gmail.com>
2025-09-26 18:07:23 -0500

    DataContext e inyeccion

---
+++ Proyecto_Aerolinea.Web/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Proyecto_Aerolinea.Web.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options)
+        {
+        }
+        /*public DbSet<User> Users { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+        public DbSet<Ticket> Tickets { get; set; }
+        public DbSet<Passenger> Passengers { get; set; }
+        public DbSet<SeatAssigment> SeatAssigments { get; set; }
+        public DbSet<Seat> Seats { get; set; }
+        public DbSet<Aircraft> Aircrafts { get; set; }
+        public DbSet<Flight> Flights { get; set; }
+        public DbSet<Airport> Airports { get; set; }*/
+    }
+}
