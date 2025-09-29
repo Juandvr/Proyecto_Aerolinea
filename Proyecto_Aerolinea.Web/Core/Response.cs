b7181177d23f846b4c8ed1dcb9aac9c6ea647f75
Juandvr <villabonajuan203@gmail.com>
2025-09-29 14:22:34 -0500

    nueva clase de response para extender la funcionalidad

--- Proyecto_Aerolinea.Web/Core/Response.cs
+++ Proyecto_Aerolinea.Web/Core/Response.cs
@@ -2,9 +2,51 @@
 {
     public class Response<T>
     {
-        public bool Succed { get; set; }
+        public bool Succeed { get; set; }
         public string? Message { get; set; }
         public List<string> Errors { get; set; } = new List<string>();
         public T? Result { get; set; }
+
+        public static Response<T> Failure(Exception ex, string message = "Ha ocurrido un error al generar al solicitud")
+        {
+            return new Response<T>
+            {
+                Succeed = false,
+                Message = message,
+                Errors = new List<string>
+                {
+                    ex.Message,
+                }
+            };
+        }
+
+        public static Response<T> Failure(string message, List<string> errors = null)
+        {
+            return new Response<T>
+            {
+                Succeed = false,
+                Message = message,
+                Errors = errors
+            };
+        }
+
+        public static Response<T> Success(T result, string message = "Tarea realizada con éxito")
+        {
+            return new Response<T>
+            {
+                Succeed = true,
+                Message = message,
+                Result = result,
+            };
+        }
+
+        public static Response<T> Success(string message = "Tarea realizada con éxito")
+        {
+            return new Response<T>
+            {
+                Succeed = true,
+                Message = message,
+            };
+        }
     }
 }
